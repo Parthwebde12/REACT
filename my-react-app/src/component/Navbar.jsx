18561f57ebc63e5e2faaa6aab32691da807a1a90
Parthwebde12 <parthwakodikar4@gmail.com>
2026-01-03 12:35:19 +0530

    deleted

---
+++ my-react-app/src/component/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import App from "../App";
+
+class Navbar extends React.Component{
+
+    render(){
+        return{
+            <h1> This is Navbar </h1>
+        }
+    }
+}
+
+export default App
---
+++ my-react-app/src/program1.jsx
@@ -0,0 +1,13 @@
+// Program1.jsx
+import React from "react";
+
+const Program1 = () => {
+  return (
+    <div>
+      <h2>Hello from Program 1!</h2>
+      <p>This is my first mini program.</p>
+    </div>
+  );
+};
+
+export default Program1;
