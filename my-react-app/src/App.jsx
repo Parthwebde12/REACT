30167ad6dea537f25e56a3e4695131e663d89bbc
Parthwebde12 <parthwakodikar4@gmail.com>
2026-01-20 21:16:12 +0530

    3rd

--- my-react-app/src/App.jsx
+++
@@ -1,5 +0,0 @@
-function car(){
-  return(
-    <h2>Hi i am a car!</h2>
-  )
-}
---
+++ myreact-tut/src/component/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "../App";
+
+class Navbar extends React.Component {
+  render() {
+    return <h2>App</h2>;
+  }
+}
+
+export default Navbar;
