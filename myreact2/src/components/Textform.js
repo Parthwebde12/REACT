fc0d94de9b384ef42194e07f0daea1811dfac88c
Parthwebde12 <parthwakodikar4@gmail.com>
2026-01-24 21:54:42 +0530

    6th day

--- myreact2/src/components/Textform.js
+++ myreact2/src/components/Textform.js
@@ -1,27 +1,72 @@
-import React,{useState} from 'react'
-
+import React, { useState } from "react";
 
 export default function Textform(props) {
-  const [text, setText] = useState('Enter text here');
-  const handleUpClick =()=>{
-    console.log("uppercase was selected");
-    setText("You have clicked on HandleUpClick")
-  }
-   const handleOnChange =(event)=>{
-    console.log("On Change");
+  const [text, setText] = useState("Enter text here");
+
+  const handleUpClick = () => {
+    setText(text.toUpperCase());
+  };
+
+  const handleOnChange = (event) => {
     setText(event.target.value);
+  };
+
+  const handleloclick = () =>{
+    setText(text.toLowerCase());
   }
-  // text ="new text" //wrong way to change text
-  // settext=("new Text"); //correct way to change the txt
+   
+  const handleclearClick =() =>{
+    setText(" ")
+  }
+
+  const property4 = () =>{
+    setText(text.match)
+  }
+
+
   return (
-    <div>
-      <h1>{props.heading}</h1>
-    
-  <div class="mb-3">
-    <label for="myBox" className="form-label"></label>
-    <textarea className="form-control"value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-  </div>    
-  <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-    </div>
-  )
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+
+        <div className="mb-3">
+          <label htmlFor="myBox" className="form-label"></label>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+
+        <button className="btn btn-primary mx-3" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-3" onClick={handleloclick}>
+          Convert to LowerCase
+        </button>
+        <button className="btn btn-primary mx-3" onClick={handleclearClick}>
+          Clear
+        </button>
+    <button className="btn btn-primary mx-3" onClick={property4}>
+               lenght
+        </button>
+      </div>
+
+      <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} charecters
+        </p>
+        <p>{0.008 * text.split(" ").length } minutes read</p>
+
+
+        <h3>Preview</h3>
+        <p>
+          {text}
+        </p>
+      </div>
+    </>
+  );
 }
